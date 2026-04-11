f6c8cb227eda9123f93dfbe4aa494c1b75abcbdc
PiyushSPatil <piyushpatil93226@gmail.com>
2026-04-11 22:31:31 +0530

    Added sample fallback model

--- inference.py
+++ inference.py
@@ -5,7 +5,7 @@ from openai import OpenAI
 # -----------------------------
 # CONFIG (STRICT)
 # -----------------------------
-MODEL_NAME = os.getenv("MODEL_NAME") or "gpt-4o-mini" 
+MODEL_NAME = os.getenv("MODEL_NAME") or "Qwen/Qwen2.5-72B-Instruct" 
 ENV_BASE_URL = os.getenv("ENV_BASE_URL", "http://localhost:7860")
 
 MAX_STEPS = 6
